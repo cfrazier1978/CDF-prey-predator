29bcdc7b479789cc41c53bf91dd2c38453618c7c
cfrazier1978 <66221343+cfrazier1978@users.noreply.github.com>
2021-12-27 14:56:41 -0500

    Update labs.py

--- labs.py
+++ labs.py
@@ -1,4 +1,6 @@
-from prey_predator_abm.config import exp
+# from prey_predator_abm.config import exp
+# model_dir = 'prey_predator_abm'
+
 from prey_predator_sd.config import exp
 
-model_dir = 'models'
+model_dir = 'prey_predator_sd'
